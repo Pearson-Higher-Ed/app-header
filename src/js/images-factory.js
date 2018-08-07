b732697a72e1be3d548d26b24d7c464a609e1dd2
Koshila Dissanayake <koshila.dissanayake@pearson.com>
2018-08-07 12:29:02 +0530

    Default Logo responsiveness when only application logo is added

--- src/js/images-factory.js
+++ src/js/images-factory.js
@@ -2,10 +2,12 @@ var logoLarge = require("../../images/logo_large.png");
 var helpHoverNormal = require("../../images/help-hover_normal.png");
 var personHoverNormal = require("../../images/person-hover_normal.png");
 var close = require("../../images/close.png");
+var pearsonIcon = require("../../images/PearsonIcon.svg")
 
 module.exports = {
     "logoLarge" : logoLarge,
     "helpHoverNormal": helpHoverNormal,
     "personHoverNormal": personHoverNormal,
-    "close": close
+    "close": close,
+    "pearsonIcon" : pearsonIcon
 };
